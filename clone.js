fab148ff160a29b01e7ac10a2a399b7f746fb596
Nrusingh prasad swain <nrusinghprasadswain@gmail.com>
2024-05-22 05:55:30 +0530

    clone.js

---
+++ clone.js
@@ -0,0 +1,8 @@
+let array1 = ["item1", "item2"];
+let array2 = array1;
+console.log("array1", array1);
+console.log("array2", array2);
+array1.push("item3");
+console.log("after pushing element to array 1");
+console.log("array1", array1);
+console.log("array2", array2);
